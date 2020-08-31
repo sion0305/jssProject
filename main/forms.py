b538caadbc94222b1b81c408e38c71f7d8302482
sion <aabbcc030421@gamil.com>
2020-08-31 12:22:44 +0900

    jasoseol Project

---
+++ main/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Jasoseol, Comment
+
+class JssForm(forms.ModelForm):
+    class Meta:
+        model = Jasoseol
+        fields = {'title', 'content',}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].label = "제목"
+        self.fields['title'].widget.attrs.update({
+            'class':'jss_title',
+            'placeholder': '제목',
+        })
+        self.fields['content'].label = "자기소개서 내용"
+        self.fields['content'].widget.attrs.update({
+            'class':'jss_content_form',
+        })
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content',)
+    
---
+++ main/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Jasoseol(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    updated_at = models.DateTimeField(auto_now = True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    jasoseol = models.ForeignKey(Jasoseol, on_delete=models.CASCADE)
